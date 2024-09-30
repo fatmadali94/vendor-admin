dbe972f96c1fb014735025c9e3b7c6c4c4d641dd
kasra1993 <kasramahsouli@gmail.com>
2024-09-30 16:02:12 +0330

    fix build errors

--- src/pages/partGroups/PartGroups.tsx
+++ src/pages/partGroups/PartGroups.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./partGroups.scss";
+import "./PartGroups.scss";
 import DataTable from "../../components/dataTable/DataTable";
 import Add from "../../components/add/Add";
 import { GridColDef } from "@mui/x-data-grid";
